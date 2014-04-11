17d4a69d2295e1a599211bf72855d0531acca3dc
William <williamterrillschultz@gmail.com>
2014-04-11 13:04:49 +0400

    some comment changes

--- learn_ruby/09_timer/timer.rb
+++ learn_ruby/09_timer/timer.rb
@@ -1,15 +1,15 @@
 class Timer
 
-  def seconds #setter
+  def seconds #getter
     @seconds = 0
   end
 
-  def seconds=(time_entered) #getter
+  def seconds=(time_entered) #setter
     @time_entered = time_entered
   end
 
-  def time_string #setter
-    hs = @time_entered / 3600   
+  def time_string #getter
+    hs = @time_entered / 3600 
     ms = (@time_entered - hs * 3600) / 60
     ss = @time_entered - hs * 3600 - ms * 60
     "#{padded(hs)}:#{padded(ms)}:#{padded(ss)}"
