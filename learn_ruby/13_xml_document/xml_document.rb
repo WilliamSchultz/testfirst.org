cfedc0b712d9a04bf92124c0dee5d37760759e88
William <williamterrillschultz@gmail.com>
2014-04-17 00:52:28 +0400

    one test left

--- learn_ruby/13_xml_document/xml_document.rb
+++ learn_ruby/13_xml_document/xml_document.rb
@@ -2,16 +2,31 @@ class XmlDocument
   
   def hello(hash={}, &block)
     if block.to_s.empty? == true
-      hash.empty? ? "<hello/>" : "<hello #{hash.keys[0]}='#{hash[:name]}'/>"
+      hash.empty? ? "<hello/>" : "<hello #{hash.keys[0]}='#{hash[hash.keys[0]]}'/>"
     else
     "<hello>#{yield}</hello>"
     end 
   end 
   
-  def goodbye
-    "<goodbye/>"
+  def goodbye(input=nil, &block)
+    if block.to_s.empty? == true
+      "<goodbye/>"
+    else 
+      "<goodbye>#{yield}</goodbye>"
+    end
   end 
   
+  def come_back(input=nil, &block)
+    if block.to_s.empty? == true
+      "<come_back/>"
+    else 
+      "<come_back>#{yield}</come_back>"
+    end
+  end
+  
+  def ok_fine(hash={})
+    "<ok_fine #{hash.keys[0]}='#{hash[hash.keys[0]]}'/>"
+  end
   
   def send(input)
     "<#{input}/>"
