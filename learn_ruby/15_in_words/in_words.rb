1cc796cef6fb1e128dddcc189c5d2acea3036bd6
William <williamterrillschultz@gmail.com>
2014-04-17 13:15:27 +0400

    15 started

---
+++ learn_ruby/15_in_words/in_words.rb
@@ -0,0 +1,12 @@
+class Fixnum
+  
+  def in_words
+    words = {0=>"zero",1=>"one",2=>"two",3=>"three",4=>"four",5=>"five",6=>"six",7=>"seven",
+                  8=>"eight",9=>"nine", 10=>"ten",11=>"eleven",12=>"twelve",13=>"thirteen",14=>"fourteen",
+                  15=>"fifteen",16=>"sixteen", 17=>"seventeen", 18=>"eighteen",19=>"nineteen",
+                  20=>"twenty",30=>"thirty",40=>"forty",50=>"fifty",60=>"sixty",70=>"seventy",77=>"seventy seven",
+                  80=>"eighty",90=>"ninety",99=>"ninety nine"}
+    words[self]
+  end 
+end 
+
