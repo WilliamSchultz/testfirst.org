27097489d25ed7ef467d0c529261b4e8fbddcd43
William <williamterrillschultz@gmail.com>
2014-04-18 12:15:44 +0400

    reads up to thousands

--- learn_ruby/15_in_words/in_words.rb
+++ learn_ruby/15_in_words/in_words.rb
@@ -1,12 +1,57 @@
-class Fixnum
+module InWords
   
-  def in_words
-    words = {0=>"zero",1=>"one",2=>"two",3=>"three",4=>"four",5=>"five",6=>"six",7=>"seven",
-                  8=>"eight",9=>"nine", 10=>"ten",11=>"eleven",12=>"twelve",13=>"thirteen",14=>"fourteen",
-                  15=>"fifteen",16=>"sixteen", 17=>"seventeen", 18=>"eighteen",19=>"nineteen",
-                  20=>"twenty",30=>"thirty",40=>"forty",50=>"fifty",60=>"sixty",70=>"seventy",77=>"seventy seven",
-                  80=>"eighty",90=>"ninety",99=>"ninety nine"}
-    words[self]
+ def in_words
+   number = self
+   
+   $one = ['', 'one',  'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+   $teen = %w(ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)
+   $ten = %w(ten twenty thirty forty fifty sixty seventy eighty ninety)
+   
+   case number
+   when 0
+     "zero"
+   when 1..9
+     $one[number]
+   when 10..19
+     $teen[number-10]
+   when 20..99
+     for_tens(number)
+   when 100..999
+     for_hundreds(number)
+   when 1000..99999
+     for_thousands(number) 
+   end
+ end
+ 
+ def for_tens(input)
+   ($ten[input.to_s.slice(-2).to_i-1] + " " + $one[input.to_s.slice(1).to_i]).strip
+ end
+ 
+ def for_hundreds(input)
+   if input.to_s.slice(1,2).to_i == 00 #last two numbers 0?
+     $one[input.to_s.slice(-3).to_i] + " " + "hundred"
+   elsif input.to_s.slice(1).to_i == 1 #for teens
+     $one[input.to_s.slice(-3).to_i] + " " + "hundred " + $teen[input.to_s.slice(2).to_i]
+   else 
+     $one[input.to_s.slice(-3).to_i] + " " + "hundred " + for_tens(input.to_s.slice(1,2).to_i) # < 20
   end 
+ end
+ 
+ def for_thousands(input)
+   if input.to_s.length.to_i == 4
+     $one[input.to_s.slice(0).to_i] + " thousand"
+   else 
+     for_tens(input.to_s.slice(0,2).to_i) + " thousand " + for_hundreds(input.to_s.slice(2,3).to_i)
+   end 
+ end 
+ 
+end
+  
+class Fixnum
+  include InWords
 end 
 
+
+
+
+   
