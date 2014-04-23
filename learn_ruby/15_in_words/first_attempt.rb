7148d8377ec10db8164bb84fe525c0589cbeea4d
William <williamterrillschultz@gmail.com>
2014-04-23 13:40:04 +0400

    better solution for 15

---
+++ learn_ruby/15_in_words/first_attempt.rb
@@ -0,0 +1,94 @@
+module InWords
+  
+ def in_words
+   number = self
+   
+   
+   $one = ['', 'one',  'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+   $teen = %w(ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)
+   $ten = %w(ten twenty thirty forty fifty sixty seventy eighty ninety)
+   
+   case number
+   when 0
+     "zero"
+   when 1..9
+     $one[number]
+   when 10..19
+     $teen[number-10]
+   when 20..99
+     for_tens(number)
+   when 100..999
+     for_hundreds(number)
+   when 1000..99999
+     for_thousands(number)
+   when 1_000_000..999_999_999
+     for_millions(number)
+   when 1_000_000_000..9_999_999_999
+     for_billions(number)
+   when 1_000_000_000_000..1_999_999_999_999
+     for_trillions(number)
+   end
+   
+ end
+ 
+ def for_tens(input)
+   ($ten[input.to_s.slice(-2).to_i-1] + " " + $one[input.to_s.slice(1).to_i]).strip
+ end
+ 
+ def for_hundreds(input)
+   if input == 000 #all numbers 0
+     ''
+   elsif input.to_s.slice(1,2) == '00' #last two numbers 0?
+     $one[input.to_s.slice(-3).to_i] + " " + "hundred"
+   elsif input.to_s.length == 1 #001 turns into 1
+     $one[input]
+   elsif input.to_s.length == 2 #001 turns into 1
+     for_tens(input)
+   elsif input.to_s.slice(1).to_i == 1 #for teens
+     $one[input.to_s.slice(-3).to_i] + " " + "hundred " + $teen[input.to_s.slice(2).to_i]
+   else 
+     $one[input.to_s.slice(-3).to_i] + " " + "hundred " + for_tens(input.to_s.slice(1,2).to_i) # > 20
+  end 
+ end
+ 
+ def for_thousands(input)
+   if input.to_s.length.to_i == 4 # > 10k
+     $one[input.to_s.slice(0).to_i] + " thousand"
+   else # < 10k
+     for_tens(input.to_s.slice(0,2).to_i) + " thousand " + for_hundreds(input.to_s.slice(2,3).to_i)
+   end 
+ end
+ 
+ def for_millions(input)
+   if input.to_s.slice(0,2).to_i < 20 # < 20m
+     $teen[input.to_s.slice(0).to_i-1] + " million" + " one"
+   else # > 20 m
+     
+   end 
+ end
+ 
+ def for_billions(input)
+   if input.to_s.slice(0,1).to_i < 10
+     $one[input.to_s.slice(0,1).to_i] + " billion " + for_hundreds(input.to_s.slice(1,3).to_i) + " million " +
+     for_hundreds(input.to_s.slice(4,3).to_i) + " thousand " + for_hundreds(input.to_s.slice(7,3).to_i)
+   end
+ end   
+ 
+ def for_trillions(input)
+   if input.to_s.length.to_i == 13 # <= 9t
+     ($one[input.to_s.slice(0,1).to_i] + " trillion " +
+     if input.to_s.slice(1,3).to_i == 0 then '' else for_hundreds(input.to_s.slice(1,3).to_i) + " billion " end +
+     if input.to_s.slice(4,3).to_i == 0 then '' else for_hundreds(input.to_s.slice(4,3).to_i) + " million " end +
+     if input.to_s.slice(7,3).to_i == 0 then '' else for_hundreds(input.to_s.slice(7,3).to_i) + " thousand " end +
+     if input.to_s.slice(10,3).to_i == 0 then '' else for_hundreds(input.to_s.slice(10,3).to_i) end).strip
+   else # >= 10t
+     
+   end 
+   
+ end 
+ 
+end
+  
+class Fixnum
+  include InWords
+end 
--- learn_ruby/15_in_words/in_words.rb
+++ learn_ruby/15_in_words/in_words.rb
@@ -1,96 +1,42 @@
 module InWords
-  
- def in_words
-   number = self
-   
-   $one = ['', 'one',  'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-   $teen = %w(ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)
-   $ten = %w(ten twenty thirty forty fifty sixty seventy eighty ninety)
-   
-   case number
-   when 0
-     "zero"
-   when 1..9
-     $one[number]
-   when 10..19
-     $teen[number-10]
-   when 20..99
-     for_tens(number)
-   when 100..999
-     for_hundreds(number)
-   when 1000..99999
-     for_thousands(number)
-   when 1_000_000..999_999_999
-     for_millions(number)
-   when 1_000_000_000..9_999_999_999
-     for_billions(number)
-   when 1_000_000_000_000..1_999_999_999_999
-     for_trillions(number)
-   end
-   
- end
- 
- def for_tens(input)
-   ($ten[input.to_s.slice(-2).to_i-1] + " " + $one[input.to_s.slice(1).to_i]).strip
- end
- 
- def for_hundreds(input)
-   if input == 000 #all numbers 0
-     ''
-   elsif input.to_s.slice(1,2) == '00' #last two numbers 0?
-     $one[input.to_s.slice(-3).to_i] + " " + "hundred"
-   elsif input.to_s.length == 1 #001 turns into 1
-     $one[input]
-   elsif input.to_s.length == 2 #001 turns into 1
-     for_tens(input)
-   elsif input.to_s.slice(1).to_i == 1 #for teens
-     $one[input.to_s.slice(-3).to_i] + " " + "hundred " + $teen[input.to_s.slice(2).to_i]
-   else 
-     $one[input.to_s.slice(-3).to_i] + " " + "hundred " + for_tens(input.to_s.slice(1,2).to_i) # > 20
-  end 
- end
- 
- def for_thousands(input)
-   if input.to_s.length.to_i == 4 # > 10k
-     $one[input.to_s.slice(0).to_i] + " thousand"
-   else # < 10k
-     for_tens(input.to_s.slice(0,2).to_i) + " thousand " + for_hundreds(input.to_s.slice(2,3).to_i)
-   end 
- end
- 
- def for_millions(input)
-   if input.to_s.slice(0,2).to_i < 20 # < 20m
-     $teen[input.to_s.slice(0).to_i-1] + " million" + " one"
-   else # > 20 m
-     
-   end 
- end
- 
- def for_billions(input)
-   if input.to_s.slice(0,1).to_i < 10
-     $one[input.to_s.slice(0,1).to_i] + " billion " + for_hundreds(input.to_s.slice(1,3).to_i) + " million " +
-     for_hundreds(input.to_s.slice(4,3).to_i) + " thousand " + for_hundreds(input.to_s.slice(7,3).to_i)
-   end
- end   
- 
- def for_trillions(input)
-   if input.to_s.length.to_i == 13 # <= 9t
-     ($one[input.to_s.slice(0,1).to_i] + " trillion " +
-     if input.to_s.slice(1,3).to_i == 0 then '' else for_hundreds(input.to_s.slice(1,3).to_i) + " billion " end +
-     if input.to_s.slice(4,3).to_i == 0 then '' else for_hundreds(input.to_s.slice(4,3).to_i) + " million " end +
-     if input.to_s.slice(7,3).to_i == 0 then '' else for_hundreds(input.to_s.slice(7,3).to_i) + " thousand " end +
-     if input.to_s.slice(10,3).to_i == 0 then '' else for_hundreds(input.to_s.slice(10,3).to_i) end).strip
-   else # >= 10t
-     
-   end 
-   
- end 
- 
-end
-  
+  def in_words
+    number = self
+    if number == 0 then return 'zero' end
+
+    ones = %w(one two three four five six seven eight nine)
+    teens = %w(ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)
+    tens = %w(twenty thirty forty fifty sixty seventy eighty ninety)
+    bigs = %w(thousand million billion trillion)
+
+    all_words = []
+    chunks = number.to_s.reverse.scan(/.{1,3}/).map { |s| s.reverse.to_i }
+
+    chunks.each_with_index do |chunk, i|
+      if chunk > 0 # if chunk is 000 nothing happens
+        words = []
+        if chunk >= 100 then words << [ones[chunk / 100 - 1], 'hundred'] end #every chunk >100 includes word 'hundred'
+          
+        if chunk % 100 > 0 #ex. if chunk is 100 nothing else happens
+          if chunk % 100 >= 10 && chunk % 100 < 20 #teens; need to be first into the words
+            words << teens[chunk % 100 - 10]
+          else 
+            if chunk % 100 >= 20 then words << tens[chunk % 100 / 10 - 2] end # >teens 
+            if chunk % 10 > 0 then words << ones[chunk % 10 - 1] end #<teens 
+          end
+        end
+
+        if i != 0 then words << bigs[i-1] end #greater than hundreds?
+        all_words << words.join(' ')#enter results of this loop into the master array
+        
+      end
+    end #ends loop
+    all_words.reverse.join(' ')
+  end #method
+end #module
+
 class Fixnum
   include InWords
-end 
+end
 
 
 
